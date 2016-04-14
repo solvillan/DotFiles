fdba98c996a9ffe0f050128f772fb415b8f28a42
Rickard Doverfelt <rickard.doverfelt@gmail.com>
2016-04-14 20:38:26 +0200

    Initial commit

---
+++ .mpcControl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+$play = `mpc playlist | dmenu -b -i -nb '#5f5f5f' -nf '#989898' -sf '#c5c5c5' -sb '#5f5f5f' -h 40 -w 1366 -x 0 -y 364 -fn '-*-terminus-medium-r-*-*-16-*-*-*-*-*-*-*' -p "    Play: " | cut -d')' -f1`
+
+[[ ! -z "${param// }" ]] && mpc play $play
